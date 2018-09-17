ee618585affc35e8481b607bb5ed5f9ba38085e4
vhoyospoyato <vhoyospoyato@gmail.com>
2018-09-17 03:12:27 +0200

    Division round up

---
+++ radius.py
@@ -0,0 +1,12 @@
+import math
+print("Hi!")
+
+prompt = 'Please enter the radius of a circule to calculate the surface: '
+
+r = input(prompt)
+
+pi = math.pi
+
+surface = pi *(int(r)**2)
+
+print ("the surface of the circle is: ", +surface)
