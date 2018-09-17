efc756a9d2773585ae952ad7387acef6807b1bd0
vhoyospoyato <vhoyospoyato@gmail.com>
2018-09-17 09:19:13 +0200

    Update radius.py

--- radius.py
+++ radius.py
@@ -2,11 +2,10 @@ import math
 print("Hi!")
 
 prompt = 'Please enter the radius of a circule to calculate the surface: '
-
 r = input(prompt)
 
 pi = math.pi
 
 surface = pi *(int(r)**2)
 
-print ("the surface of the circle is: ", +surface)
+print ("The surface of the circle is: ", +surface)
