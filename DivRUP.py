d31b87f996077a21c76d2b8f51beaf506b266251
vhoyospoyato <vhoyospoyato@gmail.com>
2018-09-17 03:00:12 +0200

    Division round up

---
+++ DivRUP.py
@@ -0,0 +1,16 @@
+import math
+
+
+print("Hi!")
+
+prompt = 'Please enter a number you want to divide: '
+
+x = input(prompt)
+
+prompt1 = 'Now enter a number to divide the first: '
+
+y = input(prompt1)
+
+division = float (x) / float(y)
+
+print("The division result is: ", + math.ceil(division))
